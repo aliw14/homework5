b457445c82d3fd2f5602776ce1a511342252c5d5
ali <ali@MacBook-Pro-Ali.local>
2022-12-28 20:51:14 +0400

    mesaj

---
+++ hpmework5/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace hpmework5;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("eded daxil et");
+        int number = int.Parse(Console.ReadLine());
+        Console.Write("eded daxil et");
+        int number2 = int.Parse(Console.ReadLine());
+
+        int count = 0;
+        
+            for (int r = number; r <=number2; r++)
+            {
+
+                if (Math.Sqrt(r)==(int)Math.Sqrt(r) ) 
+                {
+                    count++;
+                }
+            }
+
+        Console.WriteLine(count);
+
+    }
+    
+}
+
+
